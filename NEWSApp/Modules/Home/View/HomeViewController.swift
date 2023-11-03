84c0b46dd8469a8b29423c237e248cc95ed41424
Ahmed Hamam <ahmedelsayedhamam1@gmail.com>
2023-11-03 08:47:32 +0200

    add dataSource for sliderCollection and timer

--- NEWSApp/Modules/Home/View/HomeViewController.swift
+++ NEWSApp/Modules/Home/View/HomeViewController.swift
@@ -12,65 +12,47 @@ import RxDataSources
  
 class HomeViewController: UIViewController, UIScrollViewDelegate {
 
+    @IBOutlet weak var sliderCollectioView: UICollectionView!
+    
     @IBOutlet weak var newsCollectionView: UICollectionView!
+    
     let bag = DisposeBag()
     var homeViewModel: HomeViewModelProtocol?
+    lazy var counter = 0
+    var timer: Timer?
     
     lazy var dataSource = RxCollectionViewSectionedReloadDataSource<NewsSection> (configureCell: { dataSource , collectionView, indexPath, item in
-        //let section = dataSource[indexPath.section]
-        if indexPath.item == 0 {
        
-            let sliderCell = collectionView.dequeueReusableCell(withReuseIdentifier: "SliderCollectionViewCell", for: indexPath) as! SliderCollectionViewCell
-            // Configure the cell with the item from the slider section
-    sliderCell.configure(with: item)
-            return sliderCell
-        }else {
             let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "NewsCollectionViewCell", for: indexPath) as! NewsCollectionViewCell
             cell.configure(with: item)
 
             return cell
-        }
-//        switch section{
-//                case .sliderSection(_, let items):
-//                    let sliderCell = collectionView.dequeueReusableCell(withReuseIdentifier: "SliderCollectionViewCell", for: indexPath) as! SliderCollectionViewCell
-//                    // Configure the cell with the item from the slider section
-//            sliderCell.configure(with: item)
-//                    return sliderCell
-//        case .newsSection(_, let items):
-//                    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "NewsCollectionViewCell", for: indexPath) as! NewsCollectionViewCell
-//                    cell.configure(with: item)
-//
-//                    return cell
-//                }
-   return UICollectionViewCell()
-
     }, configureSupplementaryView: { dataSource, collectioView, kind, indexPath in
       
         let section = dataSource[indexPath.section]
 
         if kind == UICollectionView.elementKindSectionHeader{
             let headerC = collectioView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "HeaderCollectionReusableView", for: indexPath) as! HeaderCollectionReusableView
-            switch section {
-                     case let .sliderSection(header, _):
-                         // Configure the header view for the slider section
-                print(header)
-
-                headerC.backgroundColor = .blue
-                return headerC
-                     case let .newsSection(header, _):
-                         // Configure the header view for the news section
-                print(header)
-                headerC.backgroundColor = .red
-
+    
+               // headerC.backgroundColor = .red
                 headerC.configure()
                 return headerC
-                
             }
-//            headerC.configure()
-//            return headerC
+        return UICollectionReusableView()
         }
+    )
+    
+    
+    lazy var sliderDataSource = RxCollectionViewSectionedReloadDataSource<NewsSection> (configureCell: { dataSource , collectionView, indexPath, item in
+        //let section = dataSource[indexPath.section]
+            
+            let sliderCell = collectionView.dequeueReusableCell(withReuseIdentifier: "SliderDetailsCollectionViewCell", for: indexPath) as! SliderDetailsCollectionViewCell
+            // Configure the cell with the item from the slider section
+            sliderCell.configure(with: item)
         
-       return UICollectionReusableView()
+      //  sliderCell.dataSorcee = dataSource
+            return sliderCell
+      
     }
     )
     
@@ -79,13 +61,59 @@ class HomeViewController: UIViewController, UIScrollViewDelegate {
         title = "News"
         homeViewModel = HomeViewModel()
         prepareNavigation()
+      
       //  configureCollectioView()
         registerCell()
         bindCollectionView()
         //subscribeWithCollectionView()
         homeViewModel?.viewDidLoad()
+       
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        startTimer()
+    }
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        stopTimer()
+    }
+    private func startTimer(){
+        stopTimer()
+        guard timer == nil else {return}
+        timer = Timer.scheduledTimer(timeInterval: 3.0, target: self, selector: #selector(moveToNext), userInfo: nil, repeats: true)
+    }
+    
+    private func stopTimer(){
+        guard timer != nil else {return}
+        timer?.invalidate()
+        timer = nil
     }
 
+    @objc private func moveToNext(){
+        if let sliderArr = homeViewModel?.output.tenItems.asObservable() {
+            sliderArr
+                .map { $0.count }
+                .subscribe(onNext: { [weak self] count in
+                    guard let self else {return}
+                    if count > 0 {
+                     //   print(count)
+                        if self.counter < count - 1 {
+                            self.counter += 1
+                          //  print(self.counter)
+                        } else {
+                            self.counter = 0
+                        }
+                        self.sliderCollectioView.scrollToItem(at: IndexPath(item: self.counter, section: 0), at: .right, animated: true)
+                        
+                    }
+                })
+                .disposed(by: bag)
+        }
+        
+        homeViewModel?.viewDidLoad()
+        
+    }
+    
     private func prepareNavigation(){
         navigationController?.navigationBar.tintColor = .systemGray
 
@@ -94,20 +122,16 @@ class HomeViewController: UIViewController, UIScrollViewDelegate {
         self.navigationItem.leftBarButtonItem  = menueBarButtonItem
 
     }
-    
-//    private func configureCollectioView(){
-//        newsCollectionView.delegate = self
-//        newsCollectionView.dataSource = self
-//    }
+ 
     private func registerCell(){
-        //newsCollectionView.registerCelNib(cellClass: SliderCollectionViewCell.self)
-        newsCollectionView.register(UINib(nibName: "SliderCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "SliderCollectionViewCell")
-        newsCollectionView.registerCelNib(cellClass: NewsCollectionViewCell.self)
         
+        newsCollectionView.registerCelNib(cellClass: NewsCollectionViewCell.self)
+        sliderCollectioView.registerCelNib(cellClass: SliderDetailsCollectionViewCell.self)
         
         newsCollectionView.register(HeaderCollectionReusableView.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: "HeaderCollectionReusableView")
+        sliderCollectioView.register(HeaderCollectionReusableView.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: "HeaderCollectionReusableView")
       newsCollectionView.collectionViewLayout = layout()
-
+        sliderCollectioView.collectionViewLayout = sliderLayout()
     }
     
     @objc func onMenueButtonClicked(_ sender: Any){
@@ -132,62 +156,41 @@ class HomeViewController: UIViewController, UIScrollViewDelegate {
         layout.minimumInteritemSpacing = 5
         layout.sectionInset = UIEdgeInsets(top: 0, left: 1, bottom: 0, right: 1)   //bading between sections
         let width = view.frame.width-20
-        let height = width/2-20
-        let cellHeight: CGFloat = (dataSource.numberOfSections(in: newsCollectionView) == 1) ? 300 : 150
+        let height = CGFloat(150)
+       // let cellHeight: CGFloat = (dataSource.numberOfSections(in: newsCollectionView) == 1) ? 300 : 150
 
         layout.headerReferenceSize = CGSize(width: UIScreen.main.bounds.width, height: 50)
-        layout.itemSize = CGSize(width: width, height: cellHeight)
+        layout.itemSize = CGSize(width: width, height: height)
+     return layout
+    }
+    
+    private func sliderLayout()-> UICollectionViewFlowLayout{
+
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        layout.minimumLineSpacing = 5
+        layout.minimumInteritemSpacing = 5
+        layout.sectionInset = UIEdgeInsets(top: 0, left: 1, bottom: 0, right: 1)   //bading between sections
+        let width = view.frame.width
+        let height = CGFloat(300)
+      //  let cellHeight: CGFloat = (dataSource.numberOfSections(in: newsCollectionView) == 1) ? 300 : 150
+
+//        layout.headerReferenceSize = CGSize(width: UIScreen.main.bounds.width, height: 50)
+        layout.itemSize = CGSize(width: width, height: height)
      return layout
     }
 
     func bindCollectionView(){
-      //  newsCollectionView.rx.setDelegate(self).disposed(by: bag)
-        // Create the RxCollectionViewSectionedReloadDataSource
-        
-//        homeViewModel?.output.newssPuplish.bind(to: self.newsCollectionView.rx.items(dataSource: dataSource)).disposed(by: bag)
-        
-//        titleForHeaderInSection: { dataSorce, sectionIndex in
-//            return dataSorce[sectionIndex].model
-//        }
-        
-//        let sliderItems: [String] = ["as","sd","ff"]
-//
-//        let sliderSection = NewsSection.sliderSection(header: "hello", items: sliderItems)
-//        let newsSection = NewsSection.newsSection(header: "Test", items: newssPuplish)
-//
-//        Observable.just([sliderSection, newsSection])
-//            .bind(to: newsCollectionView.rx.items(dataSource: dataSource))
-//            .disposed(by: bag)
-        
-        homeViewModel?.output.newssPuplish.map { [NewsSection(original: .newsSection(header: "Test", items: $0), items: $0)] }
+        homeViewModel?.output.newssPuplish.map { [NewsSection(original: NewsSection(header: "", items: $0), items: $0)] }
                .bind(to: newsCollectionView.rx.items(dataSource: dataSource))
                .disposed(by: bag)
         
-//        homeViewModel?.output.newssPuplish.bind(to: newsCollectionView.rx.items(dataSource: dataSource)).disposed(by: bag)
-    
-        
-        // Bind the data to your collection view
-      
+        homeViewModel?.output.tenItems.asObservable().map{ [NewsSection(original: NewsSection(header: "", items: $0), items: $0)] }.bind(to: sliderCollectioView.rx.items(dataSource: sliderDataSource)).disposed(by: bag)
+ 
     }
 }
 //MARK: -
-//extension HomeViewController: UICollectionViewDelegate, UICollectionViewDataSource {
-//
-//    func numberOfSections(in collectionView: UICollectionView) -> Int {
-//        return 2
-//    }
-//
-//    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-//        return 4
-//    }
-//
-//    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-//        return UICollectionViewCell()
-//    }
-//
-//}
 
-//MARK: - UICollectionView
 extension HomeViewController {
    
 }
