2441359b4af344e58b1d9cf14baca069cba6902d
Ahmed Hamam <ahmedelsayedhamam1@gmail.com>
2023-11-05 05:33:19 +0200

    save in userDefaults

--- NEWSApp/Manager/FavoritesManager.swift
+++ NEWSApp/Manager/FavoritesManager.swift
@@ -5,4 +5,45 @@
 //  Created by Ahmed Hamam on 04/11/2023.
 //
 
-import Foundation
+import UIKit
+
+
+final class FavoritesManager {
+    static let shared = FavoritesManager()
+    
+    private let userDefaults = UserDefaults.standard
+    private let favoritesKey = "FavoriteArticles"
+    
+    func addFavorite(_ article: Articles) {
+        var favorites = getFavorites()
+        favorites.append(article)
+        saveFavorites(favorites)
+    }
+    
+    func removeFavorite(_ article: Articles) {
+        var favorites = getFavorites()
+        if let index = favorites.firstIndex(where: { $0.url == article.url }) {
+            favorites.remove(at: index)
+            saveFavorites(favorites)
+        }
+    }
+    
+    func isArticleFavorited(_ article: Articles) -> Bool {
+            let favorites = getFavorites()
+            return favorites.contains { $0.url == article.url }
+        }
+    
+    func getFavorites() -> [Articles] {
+        if let data = userDefaults.data(forKey: favoritesKey),
+           let favorites = try? JSONDecoder().decode([Articles].self, from: data) {
+            return favorites.reversed()
+        }
+        return []
+    }
+    
+    private func saveFavorites(_ favorites: [Articles]) {
+        if let data = try? JSONEncoder().encode(favorites) {
+            userDefaults.set(data, forKey: favoritesKey)
+        }
+    }
+}
