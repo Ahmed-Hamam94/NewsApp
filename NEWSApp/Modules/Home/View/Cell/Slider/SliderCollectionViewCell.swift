737d203b860f0572f05d28adb1883dec384d9c17
Ahmed Hamam <ahmedelsayedhamam1@gmail.com>
2023-11-05 05:28:29 +0200

    configure cell witg data

--- NEWSApp/Modules/Home/View/Cell/Slider/SliderCollectionViewCell.swift
+++ NEWSApp/Modules/Home/View/Cell/Slider/SliderCollectionViewCell.swift
@@ -6,28 +6,66 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
+import RxDataSources
+
+
 
 class SliderCollectionViewCell: UICollectionViewCell {
     
     @IBOutlet weak var collectionView: UICollectionView!
     
+    //var newssPuplish: PublishSubject<[Articles]> = .init()
+    var homeViewModel: HomeViewModelProtocol? = HomeViewModel()
+    let bag = DisposeBag()
     lazy var counter = 0
     var timer: Timer?
+    var newsArr: PublishSubject<[Articles]> = .init()
+
+    lazy var dataSource = RxCollectionViewSectionedReloadDataSource<NewsSection> (configureCell: { dataSource , collectionView, indexPath, item in
+        let sliderCell = collectionView.dequeueReusableCell(withReuseIdentifier: "SliderDetailsCollectionViewCell", for: indexPath) as! SliderDetailsCollectionViewCell
+        sliderCell.configure(with: item)
+        return sliderCell
+    })
     
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
         backgroundColor = .yellow
+        
         registerCell()
         timer = Timer.scheduledTimer(timeInterval: 3.0, target: self, selector: #selector(moveToNext), userInfo: nil, repeats: true)
+        bindToCollectionView()
         
     }
     @objc private func moveToNext(){
         
     }
+    
     func configure(with model: Articles){
+        //   print("yuyu \(model)")
+        
+        
     }
     private func registerCell(){
         collectionView.registerCelNib(cellClass: SliderDetailsCollectionViewCell.self)
+    }
+    private func bindToCollectionView(){
+        //        homeViewModel?.output.newssPuplish.bind(to: collectionView.rx.items(cellIdentifier: String(describing: SliderDetailsCollectionViewCell.self), cellType: SliderDetailsCollectionViewCell.self)) { index , article, cell in
+        //
+        ////            if let newsArr = self.homeViewModel?.output.newssPuplish{
+        ////
+        ////            }
+        //
+        //            cell.configure(with: article)
+        //
+        //        }.disposed(by: bag)
+        //    }
+        
+        
+//        homeViewModel?.output.newssPuplish.map { [NewsSection(original: .newsSection(header: "Test", items: $0), items: $0)] }
+//            .bind(to: collectionView.rx.items(dataSource: dataSource))
+//            .disposed(by: bag)
     }
 }
