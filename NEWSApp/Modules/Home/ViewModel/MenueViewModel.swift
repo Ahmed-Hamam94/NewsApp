3b44ebff59bd2c6e0723ac8d47cf682a4efde07a
Ahmed Hamam <ahmedelsayedhamam1@gmail.com>
2023-11-05 05:22:25 +0200

    add menueItems

--- NEWSApp/Modules/Home/ViewModel/MenueViewModel.swift
+++ NEWSApp/Modules/Home/ViewModel/MenueViewModel.swift
@@ -6,3 +6,30 @@
 //
 
 import Foundation
+
+
+class MenueViewModel {
+    var menueItems: [MenueCategory] = CategoryList().items
+    
+    var onSucces: (()->())?
+    var onError: ((_ errorStr: String)->())?
+    
+    init() {
+        fetchNewsData(category: "general")
+    }
+    
+    func fetchNewsData(category: String) {
+        
+    }
+    
+//    func cellForRow(at indexPath: IndexPath) -> Article? {
+//        return newsData[indexPath.row]
+//    }
+//
+//    func numberOfRowss(in section: Int) -> Int {
+//
+//        return 0
+//    }
+    
+   
+}
