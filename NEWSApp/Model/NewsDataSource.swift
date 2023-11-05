071a3cf0eceae27da395b6fae9c8425bc0770f9a
Ahmed Hamam <ahmedelsayedhamam1@gmail.com>
2023-11-05 05:32:41 +0200

    add Item

--- NEWSApp/Model/NewsDataSource.swift
+++ NEWSApp/Model/NewsDataSource.swift
@@ -7,54 +7,57 @@
 
 import RxDataSources
 
-//
-//struct NewsSection: Codable {
-//    var header: String
-//    var items: [Item]
-//}
-//
-//extension NewsSection: SectionModelType {
-//    typealias Item = Articles
-//    init(original: NewsSection, items: [Item]) {
-//        self = original
-//        self.items = items
-//    }
-//}
-enum NewsSection {
-    case sliderSection(header: String, items: [Articles])
-    case newsSection(header: String, items: [Articles])
+
+struct NewsSection: Codable {
+    var header: String
+    var items: [Item]
 }
 
 extension NewsSection: SectionModelType {
     typealias Item = Articles
-    
     init(original: NewsSection, items: [Item]) {
-        switch original {
-        case let .sliderSection(header: header, items: _):
-            self = .sliderSection(header: header, items: items)
-        case let .newsSection(header: header, items: _):
-            self = .newsSection(header: header, items: items)
-        }
-    }
-    
-    var header: String {
-        switch self {
-        case let .sliderSection(header: header, _):
-            return header
-        case let .newsSection(header: header, _):
-            return header
-        }
-    }
-    
-    var items: [Item] {
-        switch self {
-        case let .sliderSection(_, items: items):
-            return items
-        case let .newsSection(_, items: items):
-            return items
-        }
+        self = original
+        self.items = items
     }
 }
+
+
+//
+//enum NewsSection {
+//    case sliderSection(header: String, items: [Articles])
+//    case newsSection(header: String, items: [Articles])
+//}
+//
+//extension NewsSection: SectionModelType {
+//    typealias Item = Articles
+//
+//    init(original: NewsSection, items: [Item]) {
+//        switch original {
+//        case let .sliderSection(header: header, items: _):
+//            self = .sliderSection(header: header, items: items)
+//        case let .newsSection(header: header, items: _):
+//            self = .newsSection(header: header, items: items)
+//        }
+//    }
+//
+//    var header: String {
+//        switch self {
+//        case let .sliderSection(header: header, _):
+//            return header
+//        case let .newsSection(header: header, _):
+//            return header
+//        }
+//    }
+//
+//    var items: [Item] {
+//        switch self {
+//        case let .sliderSection(_, items: items):
+//            return items
+//        case let .newsSection(_, items: items):
+//            return items
+//        }
+//    }
+//}
 
 
 //
