8d8a75f61c3346e871be0df18a65a8467dac50a1
Ahmed Hamam <ahmedelsayedhamam1@gmail.com>
2023-11-05 05:30:26 +0200

    add menue category items

--- NEWSApp/Modules/Home/Model/MenueCategory.swift
+++ NEWSApp/Modules/Home/Model/MenueCategory.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+
+struct MenueCategory {
+    let id: String
+    let value: String
+}
+
+struct CategoryList {
+    var items: [MenueCategory] = [
+        MenueCategory(id: "general", value: "General"),
+        MenueCategory(id: "business", value: "Business"),
+        MenueCategory(id: "entertainment", value: "Entertainment"),
+        MenueCategory(id: "health", value: "Health"),
+        MenueCategory(id: "science", value: "Science"),
+        MenueCategory(id: "sports", value: "Sports"),
+        MenueCategory(id: "technology", value: "Technology")
+    ]
+}
