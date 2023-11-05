63dbe3d38889211c24f2b7048c62a597b75187eb
Ahmed Hamam <ahmedelsayedhamam1@gmail.com>
2023-11-05 05:29:18 +0200

    configure cell witg data

--- NEWSApp/Modules/Home/View/Cell/News/NewsCollectionViewCell.swift
+++ NEWSApp/Modules/Home/View/Cell/News/NewsCollectionViewCell.swift
@@ -21,16 +21,29 @@ class NewsCollectionViewCell: UICollectionViewCell {
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
-//       contentView.backgroundColor = .blue
-//        authorLabel.backgroundColor = .gray
-        newsImageView.backgroundColor = .brown
+
+        authorLabel.preferredMaxLayoutWidth = contentView.frame.width - newsImageView.frame.width - 20
+        newsTitleLabel.preferredMaxLayoutWidth = contentView.frame.width - newsImageView.frame.width - 20
+        
+        authorLabel.numberOfLines = 0
+        authorLabel.lineBreakMode = .byWordWrapping
+
+        newsTitleLabel.numberOfLines = 0
+        newsTitleLabel.lineBreakMode = .byTruncatingTail
+        
+        newsImageView.layer.cornerRadius = 20
     }
 
     func configure(with model: Articles){
         let imgUrl = model.urlToImage?.encodeUrl().asURL
         newsImageView.sd_setImage(with: imgUrl)
-        authorLabel.text = model.author
-        newsTitleLabel.text = model.title
+        
+        if let author = model.author {
+            let components = author.components(separatedBy: ",")
+            authorLabel.text = components.first ?? ""
+        }
+       
+        newsTitleLabel.text = model.title ?? ""
         dateLabel.text = model.publishedAt
     }
 }
