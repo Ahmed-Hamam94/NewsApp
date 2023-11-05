db9aa36b4a3ba7f6356186d6eaf5700e687fd9d8
Ahmed Hamam <ahmedelsayedhamam1@gmail.com>
2023-11-05 05:27:22 +0200

    add UI

--- NEWSApp/Modules/Home/View/Cell/Slider/SliderDetailsCollectionViewCell.swift
+++ NEWSApp/Modules/Home/View/Cell/Slider/SliderDetailsCollectionViewCell.swift
@@ -6,6 +6,9 @@
 //
 
 import UIKit
+import SDWebImage
+import RxSwift
+import RxDataSources
 
 class SliderDetailsCollectionViewCell: UICollectionViewCell {
     @IBOutlet weak var newsImage: UIImageView!
@@ -13,9 +16,29 @@ class SliderDetailsCollectionViewCell: UICollectionViewCell {
     @IBOutlet weak var authorLabel: UILabel!
     
     @IBOutlet weak var titleLabel: UILabel!
+
+    let bag = DisposeBag()
+  
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+       // backgroundColor = .cyan
+        newsImage.layer.cornerRadius = 20
+     
     }
-  
+    
+
+    
+    func configure(with model: Articles){
+       // print("yuyu \(model)")
+        let imgUrl = model.urlToImage?.encodeUrl().asURL
+        newsImage.sd_setImage(with: imgUrl)
+        
+        if let author = model.author {
+            let components = author.components(separatedBy: ",")
+            authorLabel.text = components.first ?? ""
+        }
+        titleLabel.text = model.title ?? ""
+    }
 }
